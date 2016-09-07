12846f7fb2f7c15f4131cf205040e683f3864507
Gregg Geschke <gsgeschke@gmail.com>
2016-09-07 14:14:45 -0700

    Add gulp task to run with debug port 5858

--- gulpfile.js
+++ gulpfile.js
@@ -112,3 +112,12 @@ gulp.task('watch', ['renderer'], () => {
 });
 
 gulp.task('default', ['renderer']);
+
+
+const childProcess  = require('child_process'); 
+const electron      = require('electron-prebuilt');
+
+// create the gulp task
+gulp.task('run', function () { 
+  childProcess.spawn(electron, ['--debug=5858','./app'], { stdio: 'inherit' }); 
+});
