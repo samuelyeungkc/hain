7a86f30a403a156781dac9358ad5900a41b5ab35
Heejin <monster@teamappetizer.com>
2016-12-31 22:02:52 +0900

    Refactor plugins.js, move install-related functions to plugin-installer.js

--- app/main/worker/plugins.js
+++ app/main/worker/plugins.js
@@ -7,12 +7,6 @@ const lo_reject = require('lodash.reject');
 const lo_keys = require('lodash.keys');
 const lo_isString = require('lodash.isstring');
 
-const co = require('co');
-const fs = require('fs');
-const fse = require('fs-extra');
-const path = require('path');
-const fileUtil = require('../shared/file-util');
-
 const matchUtil = require('../shared/match-util');
 const logger = require('../shared/logger');
 const prefStore = require('./pref-store');
@@ -21,6 +15,7 @@ const indexer = require('./indexer');
 const PreferencesObject = require('../shared/preferences-object');
 const helpUtil = require('./utils/help-util');
 const responseUtil = require('./utils/response-util');
+const pluginInstaller = require('./plugin-installer');
 
 const conf = require('../conf');
 
@@ -87,56 +82,8 @@ module.exports = (workerContext) => {
     logger.debug('startup: end');
   }
 
-  function removeUninstalledPlugins(listFile, removeData) {
-    if (!fs.existsSync(listFile))
-      return;
-
-    try {
-      const contents = fs.readFileSync(listFile, { encoding: 'utf8' });
-      const targetPlugins = contents.split('\n').filter((val) => (val && val.trim().length > 0));
-
-      for (const packageName of targetPlugins) {
-        const packageDir = path.join(conf.MAIN_PLUGIN_REPO, packageName);
-        fse.removeSync(packageDir);
-
-        if (removeData) {
-          const storageDir = path.join(conf.LOCAL_STORAGE_DIR, packageName);
-          const prefFile = path.join(conf.PLUGIN_PREF_DIR, packageName);
-          fse.removeSync(storageDir);
-          fse.removeSync(prefFile);
-        }
-
-        logger.debug(`${packageName} has uninstalled successfully`);
-      }
-      fse.removeSync(listFile);
-    } catch (e) {
-      logger.error(`plugin uninstall error: ${e.stack || e}`);
-    }
-  }
-
-  function movePreinstalledPlugins() {
-    return co(function* () {
-      const preinstallDir = conf.__PLUGIN_PREINSTALL_DIR;
-      if (!fs.existsSync(preinstallDir))
-        return;
-
-      const packageDirs = fs.readdirSync(preinstallDir);
-      const repoDir = conf.MAIN_PLUGIN_REPO;
-      for (const packageName of packageDirs) {
-        const srcPath = path.join(preinstallDir, packageName);
-        const destPath = path.join(repoDir, packageName);
-        yield fileUtil.move(srcPath, destPath);
-        logger.debug(`${packageName} has installed successfully`);
-      }
-    }).catch((err) => {
-      logger.error(`plugin uninstall error: ${err.stack || err}`);
-    });
-  }
-
   function* initialize() {
-    removeUninstalledPlugins(conf.__PLUGIN_UNINSTALL_LIST_FILE, true);
-    removeUninstalledPlugins(conf.__PLUGIN_UPDATE_LIST_FILE, false);
-    yield movePreinstalledPlugins();
+    yield pluginInstaller.installPreinstalledPlugins();
 
     const ret = pluginLoader.loadPlugins(generatePluginContext);
     plugins = ret.plugins;
