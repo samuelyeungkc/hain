183757536bb33b200b28657679bab4c757511552
Clint Priest <github-hain@dev.rxv.me>
2017-02-20 14:56:28 -0600

    Bugfix for Windows, changes from macos branch broke windows.

--- app/main/server/app/app-icon-protocol.js
+++ app/main/server/app/app-icon-protocol.js
@@ -8,7 +8,6 @@ const electron = require('electron');
 const protocol = electron.protocol;
 
 const conf = require('../../conf');
-const macBundleUtil = require('../../../native/mac-bundle-util');
 
 function hashPath(bundlePath) {
   return crypto.createHash('md5').update(bundlePath).digest('hex');
@@ -17,6 +16,8 @@ function hashPath(bundlePath) {
 function register() {
   if (process.platform !== 'darwin')
     return;
+
+  const macBundleUtil = require('../../../native/mac-bundle-util');
 
   protocol.registerFileProtocol('appicon', (req, callback) => {
     const cacheDir = path.join(conf.HAIN_USER_PATH, 'AppIcons');
